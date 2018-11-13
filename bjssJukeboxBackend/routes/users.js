63e5647774673d156817ad2ff92fe7251eb50f08
perjermer <pierce.morris1998@gmail.com>
2018-11-13 16:17:41 +0000

    remove unnecessary route

--- bjssJukeboxBackend/routes/users.js
+++ bjssJukeboxBackend/routes/users.js
@@ -2,17 +2,11 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
+const auth = require("../middleware/auth");
 const { User, validate } = require("../models/user");
 
-// GET
-router.get("/", async (req, res) => {
-  const user = await User.find().select("-password -isAdmin");
-
-  res.send(user);
-});
-
 // GET logged in user
-router.get("/me", async (req, res) => {
+router.get("/me", auth, async (req, res) => {
   res.send(await User.findById(req.user._id).select("-password -isAdmin"));
 });
 
@@ -38,8 +32,14 @@ router.post("/", async (req, res) => {
   res.header("x-auth-token", token).send(_.pick(user, ["_id", "username"]));
 });
 
+router.post("/login", async (req, res) => {
+  //check input
+  //compare passwords
+  //generate token
+});
+
 // PUT update user
-router.put("/me", async (req, res) => {
+router.put("/me", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -57,7 +57,7 @@ router.put("/me", async (req, res) => {
 });
 
 // DELETE
-router.delete("/me", async (req, res) => {
+router.delete("/me", auth, async (req, res) => {
   const user = await User.findByIdAndRemove(req.user._id);
   if (!user)
     return res.status(404).send("The user with the given ID was not found");
