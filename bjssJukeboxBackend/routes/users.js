c4a90532bc8b250a4c8cbed75341a0b78a11bcd6
perjermer <pierce.morris1998@gmail.com>
2018-11-13 14:45:24 +0000

    create user routes for GET, POST, PUT & DELETE

---
+++ bjssJukeboxBackend/routes/users.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const { User, validate } = require("../models/user");
+
+router.get("/", async (req, res) => {
+  const member = await Member.find().select("-password -isAdmin -isSuperAdmin");
+
+  res.send(member);
+});
+
+// MEMBER GET user with id
+router.get("/me", async (req, res) => {
+  res.send(await User.findById(req.user._id).select("-password -isAdmin"));
+});
+
+// POST new user
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ username: req.body.username });
+  if (user) return res.status(400).send("User is already registered");
+
+  user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    isAdmin: false
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
+  const token = user.generateAuthToken();
+  res.header("x-auth-token", token).send(_.pick(user, ["_id", "username"]));
+});
+
+// MEMBER PUT update member
+router.put("/me", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const { username } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(req.body.password, salt);
+  const user = await User.findByIdAndUpdate(req.user._id, {
+    username,
+    password
+  });
+
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found");
+  res.send(user);
+});
+
+// MEMBER delete
+router.delete("/me", async (req, res) => {
+  const user = await User.findByIdAndRemove(req.user._id);
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found");
+  res.send(user);
+});
+
+module.exports = router;
