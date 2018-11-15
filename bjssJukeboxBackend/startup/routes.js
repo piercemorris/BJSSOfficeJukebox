7ba02e5bd20803137ca8ab8c064cd178b92ae3c8
perjermer <pierce.morris1998@gmail.com>
2018-11-15 12:04:54 +0000

    remove old project files

--- bjssJukeboxBackend/startup/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const spotify = require("../routes/spotify");
-const users = require("../routes/users");
-
-module.exports = function(app) {
-  app.use(express.json());
-  app.use("/api/spotify/", spotify);
-  app.use("/api/users/", users);
-};
