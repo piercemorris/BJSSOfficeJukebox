290bf8c7e31920530951f1c78fbe1b0e58c3def2
James Bennett <2009bennettj@gmail.com>
2018-11-16 11:45:48 +0000

    module requires

--- bjssjukebox/routes/spotify.js
+++ bjssjukebox/routes/spotify.js
@@ -1,9 +1,16 @@
 var express = require("express");
+var request = require("request");
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
 var router = express.Router();
 
 /*
 Routes for handling spotify GET & POST requests
 */
+var client_id = 'a11348b400434a2ba1cbcf618dadf888'; // Spotify client id
+var client_secret = 'c38b37d0392f4234bc44fe635c0b4596'; // Spotify client secret
+var redirect_uri = 'http://localhost:3000/callback'; // redirect uri
 
 /*
 router.get("/login", (req, res) => {
