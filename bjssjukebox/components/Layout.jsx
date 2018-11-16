11a86000267aaa2912eb671a6c0d4ea87a640c7e
perjermer <pierce.morris1998@gmail.com>
2018-11-16 15:26:50 +0000

    add navbar and layout component

---
+++ bjssjukebox/components/Layout.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+
+class Layout extends Component {
+  state = {
+    links: [{ title: "Home", url: "/" }, { title: "About", url: "/about" }]
+  };
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>BJSS Jukebox</title>
+          <meta charSet="utf-8" />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            crossorigin="anonymous"
+          />
+          <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"
+          />
+        </Head>
+        <Navbar />
+        <div className="container">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default Layout;
---
+++ bjssjukebox/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = props => (
+  <div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <ul className="navbar-nav">
+        <li className="nav-item">example</li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navbar;
