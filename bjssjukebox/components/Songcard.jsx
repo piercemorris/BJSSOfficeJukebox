49dedd92a9abf47bc7e649b282ece509e357e8e2
perjermer <pierce.morris1998@gmail.com>
2018-11-19 11:51:22 +0000

    add base songcard component

---
+++ bjssjukebox/components/Songcard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Songcard extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>Song card</h1>
+      </div>
+    );
+  }
+}
+
+export default Songcard;
