ba005a6522555d0ace8bff38f1cc3e26bb3f6690
perjermer <pierce.morris1998@gmail.com>
2018-12-05 10:26:34 +0000

    minor changes

--- bjssjukebox/routes/spotify.js
+++ bjssjukebox/routes/spotify.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const config = require("config");
-const request = require('request');
+const request = require("request");
 const querystring = require("querystring");
 const router = express.Router();
 
-let redirect_uri = process.env.REDIRECT_URI || 'http://localhost:3000/api/spotify/callback';
+let redirect_uri =
+  process.env.REDIRECT_URI || "http://localhost:3000/api/spotify/callback";
 
 router.get("/login", (req, res) => {
   res.redirect(
@@ -12,7 +13,7 @@ router.get("/login", (req, res) => {
       querystring.stringify({
         response_type: "code",
         client_id: config.get("spotify-client-id"),
-        scope: 'user-read-private user-read-email',
+        scope: "user-read-private user-read-email",
         redirect_uri
       })
   );
@@ -21,24 +22,28 @@ router.get("/login", (req, res) => {
 router.get("/callback", (req, res) => {
   var code = req.query.code || null;
   let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
+    url: "https://accounts.spotify.com/api/token",
     form: {
       code: code,
       redirect_uri,
-      grant_type: 'authorization_code'
+      grant_type: "authorization_code"
     },
     headers: {
-      'Authorization': 'Basic ' + (new Buffer(
-        config.get("spotify-client-id") + ':' + config.get("spotify-client-secret") //change to process.env.CLIENT_ID etc...
-      ).toString('base64'))
+      Authorization:
+        "Basic " +
+        new Buffer(
+          config.get("spotify-client-id") +
+            ":" +
+            config.get("spotify-client-secret") //change to process.env.CLIENT_ID etc...
+        ).toString("base64")
     },
     json: true
-  }
+  };
   request.post(authOptions, (error, response, body) => {
-    var access_token = body.access_token
-    let uri = process.env.FRONTEND_URI || 'http://localhost:3000/search'
-    res.redirect(uri + '?access_token=' + access_token)
-  })
-})
+    var access_token = body.access_token;
+    let uri = process.env.FRONTEND_URI || "http://localhost:3000/search";
+    res.redirect(uri + "?access_token=" + access_token);
+  });
+});
 
 module.exports = router;
