1e05b79f6169c10fe09c23b5f0edec02676737af
James Bennett <2009bennettj@gmail.com>
2018-12-05 14:21:36 +0000

    appropriately resized logo. Internal CSS

--- bjssjukebox/components/Navbar.jsx
+++ bjssjukebox/components/Navbar.jsx
@@ -4,9 +4,7 @@ import Link from "next/link";
 const Navbar = props => (
   <div>
     <nav className="navbar navbar-expand-lg">
-      <p className="navbar-brand">
-        <img className="bjss-logo" src="static/BJSS-870x460-white.png"/>
-      </p>
+      <img className="navbar-brand" src="static/BJSS-870x460-white.png" style={{width:80}}/>
       <ul className="navbar-nav">
         {props.navs.map(link => (
           <Link className="nav-item" href={link.url} key={link.url}>
