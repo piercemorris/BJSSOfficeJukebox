914c0aa577455c529e0f6a0fe2f3cf8d6f87f04f
perjermer <pierce.morris1998@gmail.com>
2018-12-11 11:07:22 +0000

    add comments to user routes & update README.md

--- bjssjukebox/routes/users.js
+++ bjssjukebox/routes/users.js
@@ -5,7 +5,7 @@ const _ = require("lodash");
 const auth = require("../middleware/auth");
 const { User, validate } = require("../models/user");
 
-// GET logged in user
+// HTTP GET request to retrieve information about the currently logged in user
 router.get("/me", auth, async (req, res) => {
   const user = await User.findOne({ username: req.user.username }).select(
     "-password -isAdmin"
@@ -14,7 +14,8 @@ router.get("/me", auth, async (req, res) => {
   res.send(user);
 });
 
-// POST new user
+// HTTP POST request to create a new user account then log them in immediately
+
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -28,15 +29,19 @@ router.post("/", async (req, res) => {
     isAdmin: false
   });
 
+  // Generate a hash function to encrypt the passwords that are going to be
+  // stored in the database
+
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
+  // Generate a json web token for authenticating the user upon authorization
   const token = user.generateAuthToken();
   res.header("x-auth-token", token).send(_.pick(user, ["_id", "username"]));
 });
 
-// POST login user
+// HTTP POST request to login a user if the credentials are correct
 router.post("/login", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
@@ -44,6 +49,7 @@ router.post("/login", async (req, res) => {
   let user = await User.findOne({ username: req.body.username });
   if (!user) return res.status(400).send("Incorrect username or password");
 
+  // hashes the input password and compares to the stored password in the database
   const match = await bcrypt.compare(req.body.password, user.password);
   if (match) {
     const token = user.generateAuthToken();
@@ -51,11 +57,12 @@ router.post("/login", async (req, res) => {
   } else return res.status(400).send("Incorrect username or password");
 });
 
-// PUT update user
+// HTTP PUT request to update the currently logged in user
 router.put("/me", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Generates a salt and hashes new password
   const { username } = req.body;
   const salt = await bcrypt.genSalt(10);
   const password = await bcrypt.hash(req.body.password, salt);
@@ -69,7 +76,8 @@ router.put("/me", auth, async (req, res) => {
   res.send(user);
 });
 
-// DELETE
+// HTTP DELETE request to delete a given user
+
 router.delete("/me", auth, async (req, res) => {
   const user = await User.findByIdAndRemove(req.user._id);
   if (!user)
