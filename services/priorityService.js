50238da8baaf15a745a6cd1a989b6c4ad7269e33
perjermer <pierce.morris1998@gmail.com>
2019-02-21 15:05:31 +0000

    add all tests and algorithms

--- services/priorityService.js
+++ services/priorityService.js
@@ -1,14 +1,11 @@
 const config = require("config");
 
-function increaseUserPriority(userPriority, timeLastAdded, timeNow, maxUserPriority=1.0, timeConst=604800000) {
+function increaseUserPriority(userPriority, timeLastAdded, timeNow, maxUserPriority = 1.0, timeConst = 604800000) {
   //check if user priority is 1.0 return 1.0
   if (userPriority === maxUserPriority) return userPriority;
 
-  //check if user never addded song before
-  if (!timeLastAdded) return maxUserPriority;
-
-  const timeDifference = timeNow-timeLastAdded;
-  const percentToIncrease = timeDifference/timeConst;
+  const timeDifference = timeNow - timeLastAdded;
+  const percentToIncrease = timeDifference / timeConst;
 
   //normalise time and increase user priority
   if (percentToIncrease < maxUserPriority) {
@@ -22,19 +19,35 @@ function increaseUserPriority(userPriority, timeLastAdded, timeNow, maxUserPrior
 }
 
 function decreaseUserPriority(userPriority) {
+  //check if user priority is zero
   if (userPriority === 0) return 0;
 
-  const newPriority = userPriority-config.get('dec-count');
+  const newPriority = userPriority - config.get('dec-count');
 
+  //decrease user priority
   if (newPriority < 0) {
-    return 0;
+    return 0.0;
   } else {
     return newPriority;
   }
 }
 
-function increaseSongPriority() {
+function increaseSongPriority(songPriority, timeLastAdded, timeNow, maxPriority = 1.0, timeConst = 604800000) {
+  //check if song priority is 1.0 return 1.0
+  if (songPriority === maxPriority) return songPriority;
 
+  const timeDifference = timeNow - timeLastAdded;
+  const percentToIncrease = timeDifference / timeConst;
+
+  //normalise time and increase song priority
+  if (percentToIncrease < maxPriority) {
+    const availableIncrease = maxPriority - songPriority;
+    const amountToIncrease = availableIncrease * percentToIncrease;
+    return songPriority + amountToIncrease;
+  } else {
+    //return max song priority if time last added exceeds time to normalise
+    return maxPriority;
+  }
 }
 
 module.exports = {
