fd08f73f2592d55c9b3de5aef5bb27e7c0fa7988
perjermer <pierce.morris1998@gmail.com>
2019-02-23 13:41:07 +0000

    save token and new package

--- routes/spotify.js
+++ routes/spotify.js
@@ -3,6 +3,7 @@ const config = require("config");
 const request = require("request");
 const querystring = require("querystring");
 const router = express.Router();
+const { Spotify, validate } = require("../models/spotify");
 
 let redirect_uri =
   process.env.REDIRECT_URI || "http://localhost:3000/api/spotify/callback";
@@ -10,12 +11,12 @@ let redirect_uri =
 router.get("/login", (req, res) => {
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
-        response_type: "code",
-        client_id: config.get("spotify-client-id"),
-        scope: "user-read-private user-read-email user-read-playback-state user-read-currently-playing user-modify-playback-state",
-        redirect_uri
-      })
+    querystring.stringify({
+      response_type: "code",
+      client_id: config.get("spotify-client-id"),
+      scope: "user-read-private user-read-email user-read-playback-state user-read-currently-playing user-modify-playback-state",
+      redirect_uri
+    })
   );
 });
 
@@ -39,8 +40,19 @@ router.get("/callback", (req, res) => {
     },
     json: true
   };
-  request.post(authOptions, (error, response, body) => {
+  request.post(authOptions, async (error, response, body) => {
     var access_token = body.access_token;
+
+    /* save spotify access token */
+    const { error } = validate(access_token);
+    if (error) return res.status(400).send(error.details[0].message, ":: invalid access token");
+
+    let accessToken = new Spotify({
+      token: accessToken
+    })
+
+    accessToken = await accessToken.save();
+
     let uri = process.env.FRONTEND_URI || "http://localhost:3000/";
     res.redirect(uri + "?access_token=" + access_token);
   });
