42f41c31432a41845c9448914f174812f5949c2a
perjermer <pierce.morris1998@gmail.com>
2019-02-28 13:13:10 +0000

    new scripts and compression for next js

--- startup/routes.js
+++ startup/routes.js
@@ -5,10 +5,11 @@ const spotify = require("../routes/spotify");
 const users = require("../routes/users");
 const songs = require("../routes/songs");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(cors());
   app.use(cookieParser());
   app.use(express.json());
+  app.use(compression());
   app.use(express.static("/public"));
   app.use("/api/spotify/", spotify);
   app.use("/api/users/", users);
