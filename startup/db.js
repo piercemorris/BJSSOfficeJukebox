dc3ed39aed14afdddec8bfb896d5d7876cd7d6db
perjermer <pierce.morris1998@gmail.com>
2019-02-28 14:11:39 +0000

    add mlab addon env var

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-  const db = config.get("db-live");
+  const db = process.env.MONGODB_URI || config.get("db-live");
   mongoose.connect(db).then(() => console.log(`Connected to ${db}...`));
 };
