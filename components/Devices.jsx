fbaf9ac0ada52b87dc0ec1dae9264bb9fcac2720
perjermer <pierce.morris1998@gmail.com>
2019-03-02 12:59:11 +0000

    add handle if no devices are present

--- components/Devices.jsx
+++ components/Devices.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 
 const Devices = ({ user, devices }) => {
-  return ( 
+  console.log(devices);
+  return (
     <div id="device-select">
       <div className="user">
         <span>Playing on {user.display_name}'s Spotify</span>
       </div>
-      <div className="select-style">
-        <select>
-          {devices.map(device => (
-            <option key={device.id} value={device.id}>{device.name}</option>
-          ))}
-        </select>
-      </div>
+      {devices.length === 0
+        ?
+        <a href="https://open.spotify.com/" target="_blank">Open Spotify</a>
+        :
+        <div className="select-style">
+          <select>
+            {devices.map(device => (
+              <option key={device.id} value={device.id}>{device.name}</option>
+            ))}
+          </select>
+        </div>
+      }
     </div>
-   );
+  );
 }
- 
+
 export default Devices;
