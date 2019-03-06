4c7139abc296b4f2faebbe8cf897196c63439d38
James Bennett <2009bennettj@gmail.com>
2019-03-06 17:35:03 +0000

    Detailed Spotify authorisation

--- components/Index.jsx
+++ components/Index.jsx
@@ -6,9 +6,9 @@ class Index extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the BJSS Office Jukebox</h1>
+        <h1>Welcome to the <b>BJSS Office Jukebox</b></h1>
         <p>Your office's very own DJ, who's always taking requests!</p>
-        <h3>Features:</h3>
+        <h3><b>Features:</b></h3>
         <ul>
           <li>Search for your favourite songs from Spotify</li>
           <li>Add them to the song queue</li>
@@ -25,22 +25,36 @@ class Index extends Component {
               <li>
                 Voice commands through Alexa.
               </li>
+              <li>
+                View various interesting statistics (and judge your colleague's taste).
+              </li>
             </ul>
         </ul>
-        <h3>How to use:</h3>
-        <p>This web app will be connected to your communal area's speaker system.</p>
-        <p>In order to add songs, you will need a <i>Premium</i> Spotify account. If you do not have one, you can always interact through the communal Alexa (WIP).</p>
-        <h4>Viewing the queue:</h4>
-        <p>This does not require a Spotify account.</p>
-        <ol>
+        <hr/>
+        <h3><b>How to use:</b></h3>
+        <p>This web app will play songs through your communal area's speaker system.</p>
+        <p>In order to search for songs to add, you will need a Spotify <i>Premium</i> account. If you do not have one, you can always interact through the communal Alexa device <b>(WIP)</b>.</p>
+        <h4><i>"I <b>don't</b> have a Spotify Premium account"</i></h4>
+        <p>Many features are still available!</p>
+        <ul>
           <li>
-            Step 1.
+            <b>View the queue:</b> select 'Queue' from the top navigation bar.
           </li>
-        </ol>
-        <h4>Adding songs to the queue.</h4>
+          <li>
+            <b>View statistics:</b> select 'Stats' from the top navigation bar. <b>(WIP)</b>
+          </li>
+        </ul>
+        <h4><i>"I <b>do</b> have a Spotify Premium account"</i></h4>
         <ol>
           <li>
-              Authorise your Spotify account:
+              <b>Authorise your Spotify account:</b>
+              <ul>
+                <p>
+                  If this is your first time using the Jukebox, you will need to connect to your Spotify account. <br/>
+                  So, simple click on 'Authorise Spotify', in the upper navigation bar. <br/>
+                  This will redirect you to a screen where you can enter your Spotify details (don't worry, they are not stored anywhere on this app)<br/>
+                </p>
+              </ul>
           </li>
         </ol>
       </div>      
