0573b45086db7306118f5ab2b4b9d43272e7322c
SincoSong <psyxs2@nottingham.ac.uk>
2019-03-16 11:19:37 +0000

    build dic for songs (the song DB should be discussed before implement), therefore, the function can NOT use now!!!

--- recommandAlgorithm/createDic.py
+++ recommandAlgorithm/createDic.py
@@ -2,35 +2,35 @@ import csv
 import pymongo
 
 
-'''
+# the function should be changed before used
 def saveDic(mydict):
     with open('dict.csv', 'wb') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in mydict.items():
            writer.writerow([key, value])
-'''
 
 
-def connectDB(uri):
-    # the DB name should be change
+
+def connectDB(uri, colName):
     myclient = pymongo.MongoClient(uri)
-    mydb = myclient["jukebox"]
-    mycol = mydb["songs"]
+    mydb = myclient.jukebox
+    mycol = mydb[colName]
     return mycol.find()
 
 
-def createDic(wordList):
+def createDic(collections):
     # zip the song name to int
-    mydict = {}
-    count = 0
-    for i in range(wordList):
-        mydict[i] = count
-        count += 1
-    return mydict
+    list = []
+    for i in collections.find():
+        # the song name and key should be changed here
+        dic1 = dict.fromkeys(['username', 'songsAdded'])
+        dic1.update(username=i['username'], songsAdded=i['songsAdded'])
+        list.append(dic1)
+    dic = dict(zip(range(len(list)), list))
+    return dic
 
 
 uri = 'mongodb://public:bjssjukeboxgroup14@ds261253.mlab.com:61253/jukebox'
-songs = connectDB(uri)
+songs = connectDB(uri, "songs")
 mydict = createDic(songs)
-# saveDic(mydict)
-print(mydict)
+saveDic(mydict)
