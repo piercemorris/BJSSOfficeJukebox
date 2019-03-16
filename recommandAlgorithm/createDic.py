2275b360cd29c0643cf022e9625ead1ae802157c
SincoSong <psyxs2@nottingham.ac.uk>
2019-03-16 09:23:14 +0000

    Access the DB

--- recommandAlgorithm/createDic.py
+++ recommandAlgorithm/createDic.py
@@ -2,24 +2,35 @@ import csv
 import pymongo
 
 
-def saveDic():
+'''
+def saveDic(mydict):
     with open('dict.csv', 'wb') as csv_file:
         writer = csv.writer(csv_file)
         for key, value in mydict.items():
            writer.writerow([key, value])
+'''
 
 
-def connectDB(DBName):
+def connectDB(uri):
     # the DB name should be change
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["runoobdb"]
-    mycol = mydb["sites"]
+    myclient = pymongo.MongoClient(uri)
+    mydb = myclient["jukebox"]
+    mycol = mydb["songs"]
     return mycol.find()
 
 
 def createDic(wordList):
     # zip the song name to int
-    for
+    mydict = {}
+    count = 0
+    for i in range(wordList):
+        mydict[i] = count
+        count += 1
+    return mydict
 
 
-wordList = connectDB(DBName)
+uri = 'mongodb://public:bjssjukeboxgroup14@ds261253.mlab.com:61253/jukebox'
+songs = connectDB(uri)
+mydict = createDic(songs)
+# saveDic(mydict)
+print(mydict)
--- recommandAlgorithm/recommand.py
+++ recommandAlgorithm/recommand.py
@@ -1,23 +1,45 @@
+import csv
+import math
+
+
 # calculate the similarity of list1 and list2
 def calSim(list1, list2):
-    distance = 0;               # use the distance to calculate the distance between two list
+    distance = 0               # use the distance to calculate the distance between two list
     for i in range(len(list1)):
-
+        distance += pow((list1[i] - list2[i]),2)
+    return math.sqrt(distance)
 
 
 def songList2vec( songList ):
-
-
+    # to access the DB
+    vector = [0] * len(mydict)
+    for i in range(songList):
+        vector[(mydict[i])] = 1
+    return  vector
 
 def readDict(fileName):
     with open(fileName, 'rb') as csv_file:
         reader = csv.reader(csv_file)
-        mydict = dict(reader)
-        return mydict
+        mydict1 = dict(reader)
+    return mydict1
 
 
-def recommandSong(list, listArray[]):
+def recommandSong(list, listArray, mydict):
+    distanceList = []
+    songList = []
+    for i in listArray:
+        distanceList.append(calSim(list, i))
+
+    for i in range(10):
+        songList.append(mydict[distanceList.index(min(distanceList))])
+        distanceList[distanceList.index(min(distanceList))] = max(distanceList)
+    return songList
+
+def updateDB():
+    # update the recommand song to the DB
 
 
-newUserSongList =
-existedUserSongList[] =
+mydict = readDict("dict.csv")
+newUserSongList = songList2vec()
+existedUserSongList = songList2vec()
+recommandSong(newUserSongList, existedUserSongList, mydict)
