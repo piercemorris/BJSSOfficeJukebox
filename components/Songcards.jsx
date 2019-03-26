e3e2f08f321462af37963fc864bdd05b562b0e98
perjermer <pierce.morris1998@gmail.com>
2019-03-26 15:03:40 +0000

    tagging error hotfix

--- components/Songcards.jsx
+++ components/Songcards.jsx
@@ -266,95 +266,98 @@ class Songcards extends Component {
                               <Button onDelete={this.handleDelete} song={song} text="Remove" className="bottom" />
                             </td>
                           </tr>
+                        ))
                       }
                       <div className="no-device">
                         Playback features can changed on the device playing the music
-                    </div>
-                      }
-                </section>
-
-                    <section className="queue">
-                      <h1 className="queue-title">Up Next</h1>
-                      <table className="queue__table">
-                        <tbody>
-                          <tr className="queue__table-header">
-                            <th className="queue__table-header-img"></th>
-                            <th className="queue__table-header-title">Title</th>
-                            <th className="queue__table-header-artist">Artist</th>
-                            <th className="queue__table-header-album">Album</th>
-                            <th className="queue__table-header-username">Requested By</th>
-                            <th className="queue__table-header-priority">Priority</th>
-                            <th className="queue__table-header-button"></th>
-                          </tr>
-                          {!song.areSongsInQueue(songs) ?
-                            null
-                            :
-                            songs
-                              .filter(song => songs.indexOf(song) != 0)
-                              .map(song => (
-                                <tr key={song._id} className="queue__table__content">
-                                  <td className="queue__table-image">
-                                    <img
-                                      className="queue__table-image-img"
-                                      src={song.song.song.album.images[1].url}
-                                      alt="song in queue"
-                                    />
-                                  </td>
-                                  <td>
-                                    {song.song.song.name}
-                                    {song.song.song.explicit ?
-                                      <span className="text-box__explicit--short">E</span>
-                                      : null
-                                    }
-                                  </td>
-                                  <td>{song.song.song.artists[0].name}</td>
-                                  <td>{song.song.song.album.name}</td>
-                                  <td>{song.username}</td>
-                                  <td>{parseFloat(Math.round(song.priority * 100) / 100).toFixed(2)}</td>
-                                  <td>
-                                    <Button onDelete={this.handleDelete} song={song} text="Remove" className="bottom" />
-                                  </td>
-                                </tr>
-                              ))
-                          }
-                        </tbody>
-                      </table>
-                    </section>
-              </>
-                  :
-              <section className="authorise-page">
-                    {
-                      unauthorised ?
-                        <div className="authorise-page__text-box">
-                          <h1 className="authorise-page__heading">
-                            <span className="authorise-page__heading--main">Hey, where's the tunes?!</span>
-                            <span className="authorise-page__heading--sub">
-                              Authorise Spotify, add songs and they'll appear in the queue
-                        </span>
-                          </h1>
-                          <Link href="/api/spotify/login">
-                            <button className="authorise-page__button">
-                              Authorise Spotify
-                      </button>
-                          </Link>
-                        </div>
-                        :
-                        <div className="authorise-page__text-box">
-                          <h1 className="authorise-page__heading">
-                            <span className="authorise-page__heading--main">Hey, where's the tunes?!</span>
-                            <span className="authorise-page__heading--sub">
-                              Add songs to the Queue with the search bar!
-                        </span>
-                          </h1>
-                        </div>
-                    }
-                  </section>
+                      </div>
+                    </>
                   }
-          </>
+                  <section className="queue">
+                    <h1 className="queue-title">Up Next</h1>
+                    <table className="queue__table">
+                      <tbody>
+                        <tr className="queue__table-header">
+                          <th className="queue__table-header-img"></th>
+                          <th className="queue__table-header-title">Title</th>
+                          <th className="queue__table-header-artist">Artist</th>
+                          <th className="queue__table-header-album">Album</th>
+                          <th className="queue__table-header-username">Requested By</th>
+                          <th className="queue__table-header-priority">Priority</th>
+                          <th className="queue__table-header-button"></th>
+                        </tr>
+                        {!song.areSongsInQueue(songs) ?
+                          null
+                          :
+                          songs
+                            .filter(song => songs.indexOf(song) != 0)
+                            .map(song => (
+                              <tr key={song._id} className="queue__table__content">
+                                <td className="queue__table-image">
+                                  <img
+                                    className="queue__table-image-img"
+                                    src={song.song.song.album.images[1].url}
+                                    alt="song in queue"
+                                  />
+                                </td>
+                                <td>
+                                  {song.song.song.name}
+                                  {song.song.song.explicit ?
+                                    <span className="text-box__explicit--short">E</span>
+                                    : null
+                                  }
+                                </td>
+                                <td>{song.song.song.artists[0].name}</td>
+                                <td>{song.song.song.album.name}</td>
+                                <td>{song.username}</td>
+                                <td>{parseFloat(Math.round(song.priority * 100) / 100).toFixed(2)}</td>
+                                <td>
+                                  <Button onDelete={this.handleDelete} song={song} text="Remove" className="bottom" />
+                                </td>
+                              </tr>
+                            ))
+                        }
+                      </tbody>
+                    </table>
+                  </section>
+                </section>
+              </>
+              :
+
+              <section className="authorise-page">
+                {
+                  unauthorised ?
+                    <div className="authorise-page__text-box">
+                      <h1 className="authorise-page__heading">
+                        <span className="authorise-page__heading--main">Hey, where's the tunes?!</span>
+                        <span className="authorise-page__heading--sub">
+                          Authorise Spotify, add songs and they'll appear in the queue
+                        </span>
+                      </h1>
+                      <Link href="/api/spotify/login">
+                        <button className="authorise-page__button">
+                          Authorise Spotify
+                      </button>
+                      </Link>
+                    </div>
+                    :
+                    <div className="authorise-page__text-box">
+                      <h1 className="authorise-page__heading">
+                        <span className="authorise-page__heading--main">Hey, where's the tunes?!</span>
+                        <span className="authorise-page__heading--sub">
+                          Add songs to the Queue with the search bar!
+                        </span>
+                      </h1>
+                    </div>
                 }
+              </section>
+            }
+          </>
+        }
+        }
       </div>
     );
-          }
-        }
-        
+  }
+}
+
 export default Songcards;
