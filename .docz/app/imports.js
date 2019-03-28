818b62b3769990848a6bd613fb0077f574b1afe7
perjermer <pierce.morris1998@gmail.com>
2019-03-28 13:16:33 +0000

    update dependencies

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'alexa/README.md': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "alexa-readme" */ 'alexa/README.md'),
-  'components/doc/Error.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "components-doc-error" */ 'components/doc/Error.mdx'),
-}
