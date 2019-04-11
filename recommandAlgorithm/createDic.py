0b94802a7ed92746890fa5014a1b521c9b9cf7fc
SincoSong <psyxs2@nottingham.ac.uk>
2019-04-11 21:15:58 +0100

    Now the dictionary had been succeed for next recommand algorithm

--- recommandAlgorithm/createDic.py
+++ recommandAlgorithm/createDic.py
@@ -2,35 +2,43 @@ import csv
 import pymongo
 
 
-# the function should be changed before used
-def saveDic(mydict):
-    with open('dict.csv', 'wb') as csv_file:
-        writer = csv.writer(csv_file)
-        for key, value in mydict.items():
-           writer.writerow([key, value])
+def save_dic(my_dic):
+    try:
+        with open('dict.csv', 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=csv_columns)
+            writer.writeheader()
+            for i in my_dic:
+                writer.writerow(i)
+    except IOError:
+        print("I/O error")
 
 
+def connect_db(col_name):
+    client = pymongo.MongoClient(uri)
+    my_db = client.jukebox
+    my_col = my_db[col_name]
+    return my_col.find()
 
-def connectDB(uri, colName):
-    myclient = pymongo.MongoClient(uri)
-    mydb = myclient.jukebox
-    mycol = mydb[colName]
-    return mycol.find()
 
-
-def createDic(collections):
+def create_dic(collections):
     # zip the song name to int
-    list = []
-    for i in collections.find():
-        # the song name and key should be changed here
-        dic1 = dict.fromkeys(['username', 'songsAdded'])
-        dic1.update(username=i['username'], songsAdded=i['songsAdded'])
-        list.append(dic1)
-    dic = dict(zip(range(len(list)), list))
-    return dic
+    list_song_dict = []               # store the song as dict into the list
+    for i in collections:
+        # the song id and feature were stored as a dictionary
+        tmp = {"songID": i['songID'], 'acousticness': i['acousticness'], 'danceability': i['danceability'],
+               'energy': i['energy'], 'instrumentalness': i['instrumentalness'], 'liveness': i['liveness'],
+               'loudness': i['loudness'], 'speechiness': i['speechiness'], 'valence': i['valence'], "tempo": i['tempo']}
+        list_song_dict.append(tmp)
+    return list_song_dict
 
 
-uri = 'mongodb://public:bjssjukeboxgroup14@ds261253.mlab.com:61253/jukebox'
-songs = connectDB(uri, "songs")
-mydict = createDic(songs)
-saveDic(mydict)
+# uri = 'mongodb://jukebox:password1@ds261253.mlab.com:61253/jukebox'
+uri = "mongodb://public:bjssjukeboxgroup14@ds261253.mlab.com:61253/jukebox"
+csv_columns = ["songID", 'acousticness', 'danceability',  'energy', 'instrumentalness', 'liveness',
+               'loudness', 'speechiness', 'valence', "tempo"]
+songs = connect_db("histories")
+my_dicts = create_dic(songs)
+save_dic(my_dicts)
+
+
+
